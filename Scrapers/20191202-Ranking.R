cb94203e1caef1fb895fc30f0a8678fb54b8ae63
Bastián González-Bustamante <bastian.gonzalezbustamante@politics.ox.ac.uk>
2021-03-01 17:42:38 +0000

    Update 20191202-Ranking.R

--- Scrapers/20191202-Ranking.R
+++ Scrapers/20191202-Ranking.R
@@ -6,17 +6,20 @@
 ## Bastián González-Bustamante
 ## University of Oxford
 ## E-mail: bastian.gonzalezbustamante@politics.ox.ac.uk
-## Website: http://users.ox.ac.uk/~shil5311/
+## Website: https://bgonzalezbustamante.com/
 
 ## Chilean Political Science Impact Ranking Project
 ## OSF-Project DOI: 10.17605/OSF.IO/C8PRA
-## Website: http://users.ox.ac.uk/~shil5311/ranking/
+## Website: https://bgonzalezbustamante.com/cps-ranking/
 ## GitHub Repository: https://github.com/bgonzalezbustamante/CPS-Ranking
 
 ##############################################################
 
 ## Package
 library(scholar)
+
+## Directory
+setwd("20191202")
 
 ## Sample December 2019
 POHGEtYAAAAJ <- get_profile('POHGEtYAAAAJ') ## Alejandro Olivares L.
@@ -149,8 +152,8 @@ rIbm6tMAAAAJ <- get_profile('rIbm6tMAAAAJ') ## Miguel Angel Lopez
 ## Authors who had added to their profile publications that are not their 
 ## authorship (because of name confusion, deliberately or without realising it) 
 ## have been removed from the ranking.
-## MMCj-VQAAAAJ <- get_profile('MMCj-VQAAAAJ') ## Álvaro Ramírez-Alujas
-## QkIdjasAAAAJ <- get_profile('QkIdjasAAAAJ') ## Juan Carlos Castillo 
+MMCj-VQAAAAJ <- get_profile('MMCj-VQAAAAJ') ## Álvaro Ramírez-Alujas
+QkIdjasAAAAJ <- get_profile('QkIdjasAAAAJ') ## Juan Carlos Castillo 
 
 ## Names
 Name <- c(POHGEtYAAAAJ$name, ## Alejandro Olivares L.
