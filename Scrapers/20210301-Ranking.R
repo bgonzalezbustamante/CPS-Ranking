ccaf96258267820e1bf45725b123628e54109637
Bastián González-Bustamante <bastian.gonzalezbustamante@politics.ox.ac.uk>
2021-09-01 16:57:27 +0100

    Update 20210301-Ranking.R

--- Scrapers/20210301-Ranking.R
+++ Scrapers/20210301-Ranking.R
@@ -225,7 +225,7 @@ Name <- c(POHGEtYAAAAJ$name, ## Alejandro Olivares L.
           nj4yvK0AAAAJ$name, ## Claudia Heiss
           ckIjzZQAAAAJ$name, ## Claudio Fuentes
           vLROxScAAAAJ$name, ## Claudio Fuentes-González
-          "Cristian Garay Vera", ## Cristian Garay Vera
+          "Cristian Garay Vera", ## QQq0cAAAAJ$name
           "Cristian Pérez Muñoz", ## MKayIWEAAAAJ$name, 
           HVy8iAAAAAJ$name, ## Cristian Pliscoff
           MwnsXycAAAAJ$name, ## Cristina Moyano Barahona
@@ -326,10 +326,10 @@ Name <- c(POHGEtYAAAAJ$name, ## Alejandro Olivares L.
           ie78OmcAAAAJ$name, ## José Miguel Cabezas
           cAowpkAAAAJ$name, ## Rodrigo Mardones
           "Macarena Valenzuela Beltrán", ## xP7heoAAAAJ$name 
-          "Germán Bidegain", ## Germán Bidegain
-          "Victor Tricot Salomon", ## Victor Tricot Salomon
-          "Adrian Albala", ## Adrian Albala
-          "Miguel Angel Lopez", ## Miguel Angel Lopez
+          "Germán Bidegain", ## RBv0BPIAAAAJ$name
+          "Victor Tricot Salomon", ## O7DOqCcAAAAJ$name
+          "Adrian Albala", ## KZhWCqgAAAAJ$name
+          "Miguel Angel Lopez", ## rIbm6tMAAAAJ$name
           qdm2fHgAAAAJ$name, ## Marcelo Mella Polanco
           dJKWN8wAAAAJ$name,  ## Hary Hugo Fruhling
           TzPYdWsAAAAJ$name, ## Arturo Arriagada
@@ -351,8 +351,8 @@ Name <- c(POHGEtYAAAAJ$name, ## Alejandro Olivares L.
           ShRtBhMAAAAJ$name, ## Lucas Perelló
           P3MuwAAAAJ$name, ## Carlos Cantillana
           R7MsicAAAAJ$name, ## Rodolfo de Camargo Lima
-          "María Patricia Sotomayor", ## María Patricia Sotomayor Valarezo
-          "Sergio Huertas-Hernandez", ## Sergio A. Huertas-Hernandez
+          "María Patricia Sotomayor", ## ZRf8dtAAAAAJ$name
+          "Sergio Huertas-Hernandez", ## fv7N0eUAAAAJ$name
           n0UQqa4AAAAJ$name, ## Aldo Madariaga
           ArIMp5YAAAAJ$name, ## Carla Cisternas 
           bjloTEAAAAJ$name, ## Armando Cartes Montory
